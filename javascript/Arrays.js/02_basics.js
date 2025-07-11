c76935b76116c4a8938242e8e9eff01fdc098528
avinash <avinash.kubsad@goselfserve.in>
2025-07-11 21:55:52 +0530

    to main branchkk

--- javascript/Arrays.js/02_basics.js
+++ javascript/Arrays.js/02_basics.js
@@ -24,6 +24,7 @@ const another_array = [1,2,3, [4,5,6],7,[6,7,[4,5]]]
 const real_another_array = another_array.flat(Infinity)
 
 console.log(real_another_array);
+console.log("real_another_array",real_another_array)
 
 
 
