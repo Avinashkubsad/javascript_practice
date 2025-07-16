2b9be165851171275d58039ce6a2387f2fc9459e
Avinashkubsad <avi.kubsad14@gmail.com>
2025-07-16 08:20:50 +0530

    parent & child function

--- javascript/functions.js/scope.js
+++ javascript/functions.js/scope.js
@@ -12,3 +12,19 @@ var c =30;
  //console.log(a)
    // console.log(b)
     console.log(c);
+
+//*note*   if there is a parent function & child function then child function can access  parent function variables. 
+function first(){
+  const name ="Avinash"
+
+  function two (){
+    const email = "avi@gmail.com"
+    console.log(name)
+  }
+ // console.log(email);
+  two()
+
+}
+
+
+first()
