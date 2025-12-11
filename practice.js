e6e5a341de48cfc6d9e1ee6110f6eeadd2d62169
Avinashkubsad <avi.kubsad14@gmail.com>
2025-12-11 22:29:05 +0530

    using loops

--- practice.js
+++ practice.js
@@ -15,9 +15,22 @@
 // )
 
 
+// function reverseString(str){
+//     if(str === '') return ""
+//     return reverseString(str.substr(1)) + str[0];
+// }
+
+// console.log(reverseString("pawan"));
+
+
+
 function reverseString(str){
-    if(str === '') return ""
-    return reverseString(str.substr(1)) + str[0];
+    let reversed ="";
+    for(let i=str.length -1; i>=0; i--) {
+         reversed +=str[i];
+    }
+
+    return reversed;
 }
 
-console.log(reverseString("Avinash"));
+console.log(reverseString("pradeep"));
