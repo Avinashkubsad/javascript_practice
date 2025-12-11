317dbb3a03557bf126b81cb0097cc395934a9c11
Avinashkubsad <avi.kubsad14@gmail.com>
2025-12-11 21:16:19 +0530

    using recursion

--- practice.js
+++ practice.js
@@ -1,15 +1,23 @@
-// the below is using function 
-function ReverseString(str){
-    return str.split('').reverse('').join('');
-}
+// // the below is using function 
+// function ReverseString(str){
+//     return str.split('').reverse('').join('');
+// }
 
-console.log(ReverseString("Avinash"))
+// console.log(ReverseString("Avinash"))
 
 
 // the below is usng spread operator 
-function RevereseString (str){
-    return [...str].reverse().join();
+// function RevereseString (str){
+//     return [...str].reverse().join();
+// }
+
+// console.log(RevereseString("Avinash")
+// )
+
+
+function reverseString(str){
+    if(str === '') return ""
+    return reverseString(str.substr(1)) + str[0];
 }
 
-console.log(RevereseString("Avinash")
-)
+console.log(reverseString("Avinash"));
