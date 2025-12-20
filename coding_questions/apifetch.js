390c87361964a8a33bda2aeff7bf006e2c78c04e
Avinashkubsad <avi.kubsad14@gmail.com>
2025-12-20 13:23:57 +0530

    apifetch

--- coding_questions/apifetch.js
+++ coding_questions/apifetch.js
@@ -1,6 +1,6 @@
 
-// 1. fetch method using tehn and catch  
+// 1. fetch method using then and catch methods
 fetch('https://jsonplaceholder.typicode.com/todos/2')
       .then(response => response.json())
       .then(json => console.log(json))
-      .catch(error => console.log(error))
+      .catch(error => console.log(error)) 
