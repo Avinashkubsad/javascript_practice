74850ab0758fccfb748fca0e011c453b5ef3dee2
Avinashkubsad <avi.kubsad14@gmail.com>
2025-12-20 12:58:12 +0530

    arrow funtions

--- javascript/functions.js/arrowfunctions.js
+++ javascript/functions.js/arrowfunctions.js
@@ -2,4 +2,10 @@ let test = ()=>{
     console.log("This is an arrow function");
 }
 
+
+let test1 = (a) => { // here a is parameter
+    console.log(a+2); 
+
+}
 test();
+test1(2,3);  // here 2 is argument
