4a9576aea46400aeb68a7b75ed42432d3ae9e5b8
Avinashkubsad <avi.kubsad14@gmail.com>
2025-12-21 12:15:03 +0530

    destructuriing

--- javascript/destructuring.js
+++ javascript/destructuring.js
@@ -1,2 +1,12 @@
 //The destructuring assignment syntax is  a javacript expression that makes it posssible to unpack values
 //from arrays or properties from objects into distinct variables.
+
+
+let arr = [10, 20, 30];
+
+// Destructuring assignment from array
+let [a, b, c] = arr;
+
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30   
